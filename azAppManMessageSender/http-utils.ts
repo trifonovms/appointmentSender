f5e4a9719701d115e43fcd30f139faafa486a7e2
vn0z4ct <mikhail.trifonov@walmart.com>
2022-12-21 20:35:00 -0500

    added support object type and base 64

--- azAppManMessageSender/http-utils.ts
+++ azAppManMessageSender/http-utils.ts
@@ -1,12 +1,43 @@
 import axios, { AxiosRequestConfig } from 'axios';
+import { Context } from "@azure/functions"
 
-export const fetchPost =async (url:string, data:string, option:any) => {
+
+export const fetchPost =async (context,url:string, data:string, option:any) => {
     try {
         const response = await axios.post(url, data, option);
         return response.data;
     } catch (error) {
-      console.error(error);
+      context.log.error(error);
+      throw error;
     }    
 }
 
 
+export const sendTGMessage = async (context: Context, chatId: number, message: string)=> {
+  const msg = {
+    chat_id:chatId,
+    text: message,
+  };
+  const token = process.env.TELEGRAM_BOT_TOKEN;
+  if(!token){
+    throw new Error('Bot token not defined');
+  }
+
+  const url = `https://api.telegram.org/bot${token}/sendMessage`;
+  const option: AxiosRequestConfig = {
+    headers:{
+      'content-type': 'application/json',
+      'connection': 'keep-alive',
+    }
+
+  }
+  const data = JSON.stringify(msg);
+  try {
+    const response = await fetchPost(context, url, data, option);
+    return response.data;
+  } catch (error) {
+    context.log.error(`sendMessage. ${error}`);
+    throw error;
+  }
+
+}
--- azAppManMessageSender/index.ts
+++ azAppManMessageSender/index.ts
@@ -1,55 +1,14 @@
 import { AzureFunction, Context } from "@azure/functions"
-import {fetchPost} from './http-utils';
-import {AxiosRequestConfig} from 'axios';
-import { METHODS } from "http";
+import {sendTGMessage} from './http-utils';
 
-const queueTrigger: AzureFunction = async function (context: Context, myQueueItem: string): Promise<void> {
-  console.log(myQueueItem);
-  const obj = {
-    userId:12345,
-    message:'`Default message` \n \n We have appointment for you at 12/12/2022 09:30AM on Flemington, NJ ',
-    link: "http://myappointment.com/12/12/2022/09/15/dmv/12"
-};
-  let msg = obj;
+const queueTrigger: AzureFunction = async function (context: Context, queueItem: any): Promise<void> {
   try {
-    msg = JSON.parse(myQueueItem);
+    await sendTGMessage(context, 1374826521,`${queueItem.message} Link: ${queueItem.link}`);
   } catch (error) {
-    msg = obj;
-  } 
-
-  try {
-    await sendMessage(1374826521,`${msg.message} Link: ${msg.link}`);
-  } catch (error) {
-    console.log(error);
+    context.log.error(error);
+    throw error;
   }
 };
 
-const sendMessage = async (chatId: number, message: string)=> {
-  const msg = {
-    chat_id:chatId,
-    text: message,
-  };
-  const token = process.env.TELEGRAM_BOT_TOKEN;
-  if(!token){
-    throw new Error('Bot token not defined');
-  }
-
-  const url = `https://api.telegram.org/bot${token}/sendMessage`;
-  const option: AxiosRequestConfig = {
-    headers:{
-      'content-type': 'application/json',
-      'connection': 'keep-alive',
-    }
-
-  }
-  const data = JSON.stringify(msg);
-  try {
-    const response = await fetchPost(url, data, option);
-    return response.data;
-  } catch (error) {
-    console.error(`sendMessage. ${error}`);
-  }
-
-}
 
 export default queueTrigger;
